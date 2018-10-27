e6f4e83978cf6f34a41eda41dedbb63f5dd25114
JuanPabloP <mail@jppp.uu.ma>
2018-10-27 15:54:21 -0500

    Incluido ngrok.exe, registro completo y funcionando

--- backend/garbj/serializers.py
+++ backend/garbj/serializers.py
@@ -14,14 +14,14 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'nombre', 'apellido','correo', 'fecha_nacimiento')
-        def create(self, validated_data, instance=None):
-            user_data = validated_data.pop('user')
-            user = User.objects.create(**user_data)
-            user.set_password(user_data['password'])
-            user.save()
-            Usuario.objects.update_or_create(user=user,**validated_data)
-            usuario = Usuario.objects.get(user=user)
-            return usuario
+    def create(self, validated_data, instance=None):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        user.set_password(user_data['password'])
+        user.save()
+        Usuario.objects.update_or_create(user=user,**validated_data)
+        usuario = Usuario.objects.get(user=user)
+        return usuario
 
 class PremioSerializer(serializers.ModelSerializer):
     class Meta:
--- frontend/garbj/src/providers/rest/rest.ts
+++ frontend/garbj/src/providers/rest/rest.ts
@@ -7,10 +7,10 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RestProvider {
-  apiUrl = 'http://71e39e6a.ngrok.io/';
+  apiUrl = 'http://33215653.ngrok.io/';
   loginService = 'api/login/';
   apiPremio = 'premio';
-
+  apiUsuarios = 'usuarios/';
 
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
@@ -27,6 +27,17 @@ export class RestProvider {
     });
   }
 
+  registro(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl + this.apiUsuarios, data)
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
   getPremio() {
     return new Promise(resolve => {
       this.http.get(this.apiUrl + this.apiPremio, {
@@ -35,7 +46,7 @@ export class RestProvider {
         resolve(data);
       }, err => {
         console.log(err);
-      }); 
+      });
     });
   }
 }
